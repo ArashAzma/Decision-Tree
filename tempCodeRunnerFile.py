ca85dcb45d1ebb409f21920ade2a12764cfd8a27
ArashAzma <arashazma3@gmail.com>
2024-01-25 13:22:38 +0330

    dsadsad

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,4 @@
-import numpy as np 
-import pandas as pd 
-from functions import entropy
-
-
-# print('start')
+)
 # df = pd.read_csv('D:\\Downloads\\UNI 3\\Data Structure\\Decision Tree\\dataset\\feature_train.csv')
 # print(df.head())
 
@@ -16,7 +11,3 @@ from functions import entropy
 #     print(f"{column}: {num_distinct_values} distinct values")
 
 # print('end')
-
-h = ['m', 'm', 'm', 'f']
-
-print(entropy(h))
