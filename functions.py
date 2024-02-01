2ee02ef5d7520be6e233f77a78ebf2aa5406e032
baharmohammadi <baharmohammadisani83@gmail.com>
2024-02-01 16:32:24 +0330

    add information gain

--- functions.py
+++ functions.py
@@ -6,7 +6,7 @@ def entropy(labels):
     j = 0
     outputs = []
     percents = []
-
+    
     for x in labels:
         found = False
         j = 0
@@ -22,19 +22,47 @@ def entropy(labels):
             i += 1
 
     for x in percents:
-        px = x / len(labels)
-        ent += px * np.log2(px)
+        ent += (x / len(labels)) * np.log2(x / len(labels))
             
     return -ent  
 
-def informationGain(EParent, WChildren,EChildren):
-    g = 0
+
+
+
+
+def informationGain(labels, data):
+    g = entropy(labels)
+    dataTypes = []
+    percents = []
+    dataEntropy = []
     i = 0
     
-    for x in WChildren:
-        g+=x*EChildren[i]
-        i+=1
-
-    g = EParent - g
+    # finding data types
+    for x in data :
+        found = False
+        for y in dataTypes :
+            if(x == y) :
+                found = True
+                break
+            else :
+                i+=1
+        if(not found) :
+            dataTypes[i] = x
+    ###
+    
+    #finding child entropies and number of each data type
+    
+    for x in range (0,len(dataTypes)-1) :
+        datalabels = []
+        for y in range (0, len (data) - 1):         # 0 ya 1 ro motmaen nistm (bastegi dare)
+            if(data[y] == dataTypes[x]):
+                percents[x] += 1
+                datalabels.append(labels[y])
+        dataEntropy[x] = entropy(datalabels)
+    
+    ###
+        
+    for x in range (0, len(dataTypes)-1) :
+        g-=(percents[x]/len(labels))*dataEntropy[x]
     
     return g
--- main.py
+++ main.py
@@ -2,13 +2,21 @@ import numpy as np
 from Tree import Tree
 import pandas as pd 
 from functions import entropy
+from functions import informationGain
 
 # inaro ham tagheer bede adresesho 
-dfFeature = pd.read_csv('D:\\Downloads\\UNI 3\\Data Structure\\Decision Tree\\dataset\\feature_train.csv', nrows=10)
-dfLabel = pd.read_csv('D:\\Downloads\\UNI 3\\Data Structure\\Decision Tree\\dataset\\label_train.csv', nrows=10)
+#dfFeature = pd.read_csv('D:\\Downloads\\UNI 3\\Data Structure\\Decision Tree\\dataset\\feature_train.csv', nrows=10)
+#dfLabel = pd.read_csv('D:\\Downloads\\UNI 3\\Data Structure\\Decision Tree\\dataset\\label_train.csv', nrows=10)
 
-data = dfFeature
-label = dfLabel['Diabetes_012']
+#data = dfFeature
+#label = dfLabel['Diabetes_012']
 
-tree = Tree()
-tree.createTree(data, label)
+#tree = Tree()
+#tree.createTree(data, label)
+
+label = ["no", "no", "yes", "yes", "yes"]
+
+data = ["w", "s", "w", "s", "w"]
+
+s = informationGain(label, data)
+print(s)
